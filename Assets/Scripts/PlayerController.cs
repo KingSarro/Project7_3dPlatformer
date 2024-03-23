b6a192b1a2270610c9e25aa99d64db2b14de7734
Sharri'a Sneed <frostellachery26@gmail.com>
2024-03-22 19:59:57 -0500

    Jumping Works
    Finished video 6 from week 6, Forces and Raycast

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,21 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour{
-    Rigidbody rb;
-    [SerializeField] float speed = 1.0f;
-    float vMovement;
-    float hMovement;
+    //======Initializations======//
+    //=----Walking----=//
+    //Initializes variables for movement 
+    private Rigidbody rb; //A reference to the rigidbody attached to this object
+    [SerializeField] float speed = 5.0f; //player speed
+    private float vMovement; //vertical movement
+    private float hMovement; //horizontal movement
+    //=----Jumping----=//
+    [SerializeField] float jumpForce = 10.0f; //player jump force
+    [SerializeField][Range(0.1f, 2.0f)] float rayLength = 0.8f;
+    private bool onGround = false;
+    [SerializeField] LayerMask enviornmentLayers; // used to specify the layers allowed to jump off of
+
+
     // Start is called before the first frame update
     private void Start(){
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>(); //Saves the rigidbody attached to this object to this reference
     }
 
     // Update is called once per frame
     private void Update(){
         //Vertical Movement
-        vMovement = Input.GetAxis("Vertical");
+        vMovement = Input.GetAxis("Vertical"); //Get
         //Horizontal Movement
         hMovement = Input.GetAxis("Horizontal");
+
+        //Checking for the jump input and if player is on ground
+        if(Input.GetButtonDown("Jump") && onGround){
+            rb.AddForce((Vector3.up * jumpForce), ForceMode.Impulse); //Add force to the rigidbody
+        }
+
+        //Drays a debug line to visualize the raycast position
+        Debug.DrawLine(transform.position, (transform.position + (Vector3.up * -rayLength)), Color.red);//Draws a ray down
+        onGround = (Physics.Raycast(transform.position, -Vector3.up, rayLength, enviornmentLayers)); //If Raycast returns true, the onGround is true. vise-versa
+
     }
 
     //Rigidbody stuff should go in fixed update
